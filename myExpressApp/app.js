facc3a81fd2bbc84da5c38a614a628b8114450be
fabiansj0028 <fabiansj0028@gmail.com>
2023-04-05 18:38:18 -0600

    Login

--- myExpressApp/app.js
+++ myExpressApp/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require("cors");
 var usersRouter = require('./routes/users');
 var dbRouter = require('./routes/database');
 
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 app.use('/', dbRouter);
 app.use('/users', usersRouter);
 
--- myExpressApp/routes/database.js
+++ myExpressApp/routes/database.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var sqlcon = require('mssql');
 var router = express.Router();
+var cors = require("cors");
 
 // configuracion de la db
 const config = {
@@ -24,25 +25,28 @@ sqlcon.connect(config, err => {
 
 //ruta base
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', cors(), function(req, res, next) {
     res.render('index', { title: 'Express' });
   });
   
 //ruta de prueba 
-router.get('/estudiantes', (req, res) => {
-    // Crear una nueva consulta a la base de datos
-    const consulta = new sqlcon.Request();
-    
-    // Ejecutar la consulta
-    consulta.query('SELECT * FROM estudiantes', (err, resultado) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Error al realizar la consulta');
-      } else {
-        res.send(resultado.recordset);
-      }
-      console.log('Consulta realizada');
-    });
+router.post('/', async(req,res)=>{
+  const {email, password} = req.body
+  const check = new sqlcon.Request();
+  check.query(`Select U.id FRom Usuarios U
+  Inner Join TiposUsuario TU ON U.idTipoUsuario = TU.id
+  where TU.id = 3 AND u.correo = '${email}' AND U.clave = '${password}'`, (err,result)=>{
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error al realizar la consulta')
+    } else {
+      console.log(req.cookies)
+      res.send(result.recordset)
+    }
+    console.log('Consulta realizada');
   });
+  
+
+});
 
 module.exports = router;
