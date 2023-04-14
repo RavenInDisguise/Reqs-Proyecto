4f575d3e8f612b1cc0e053c4a830e4ae711bc07c
psluk <p7285@outlook.com>
2023-04-14 00:37:46 -0600

    Página de React para el error 404

---
+++ myExpressApp/client/src/Error404.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import './Tarjeta.css';
+
+export default function Error404() {
+    return (
+        <div className="tarjeta">
+            <h1>Página no encontrada</h1>
+            <p>No se puedo encontrar la página solicitada</p>
+        </div>
+    )
+}
