5bdcd1d311f23017d0948c2dd6495cb630db5cb4
psluk <p7285@outlook.com>
2023-05-18 16:23:47 -0600

    Formato

--- myExpressApp/routes/login.js
+++ myExpressApp/routes/login.js
@@ -1,51 +1,48 @@
 var express = require('express');
 var router = express.Router();
 const sqlcon = require('./database.js');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 const manejarError = require('./errores.js');
 
-// Autenticación
-
-//ruta base
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('index');
 });
 
 // Verificar si hay una sesión iniciada
 router.get('/api/login', (req, res) => {
-  const saved = req.session.user;
+    const saved = req.session.user;
 
-  if (saved) {
-    res.send({
-      loggedIn : true,
-      userId : saved.userId,
-      email : saved.email,
-      idEstudiante: saved.idEstudiante,
-      tipoUsuario: saved.tipoUsuario
-    });
-  } else {
-    res.send({
-      loggedIn : false
-    });
-  }
+    if (saved) {
+        res.send({
+            loggedIn: true,
+            userId: saved.userId,
+            email: saved.email,
+            idEstudiante: saved.idEstudiante,
+            tipoUsuario: saved.tipoUsuario
+        });
+    } else {
+        res.send({
+            loggedIn: false
+        });
+    }
 });
 
-  // Cierre de sesión
-  router.get('/api/logout', (req, res) => {
+// Cierre de sesión
+router.get('/api/logout', (req, res) => {
     const saved = req.session.user;
-  
+
     if (saved) {
-      req.session.user = null;
-      res.send({
-        message : "Ok"
-      });
+        req.session.user = null;
+        res.send({
+            message: "Ok"
+        });
     } else {
-      res.status(401).send({
-        message: 'No hay ninguna sesión activa'
-      });
+        res.status(401).send({
+            message: 'No hay ninguna sesión activa'
+        });
     }
-  });
+});
   
 // Inicio de sesión
 router.post('/api/login', async (req, res) => {
@@ -55,13 +52,16 @@ router.post('/api/login', async (req, res) => {
     // Parámetros de entrada
     request.input('IN_email', sqlcon.VarChar, email);
 
-    request.execute(`BiblioTEC_SP_Login`, (error, result) => {
+    request.execute('BiblioTEC_SP_Login', (error, result) => {
         if (error) {
             manejarError(res, error);
         } else {
             if (result.recordset.length > 0) {
                 bcrypt.compare(password, result.recordset[0].clave, (error, response) => {
-                    if (response) {
+                    if (error) {
+                        console.log(error);
+                        res.status(500).send({ message: 'Ocurrió un error inesperado '});
+                    } else if (response) {
                         /* Coincide */
                         req.session.user = {
                             userId: result.recordset[0].UsuarioID,
@@ -82,7 +82,5 @@ router.post('/api/login', async (req, res) => {
         }
     });
 });
-
-
 
 module.exports = router;
