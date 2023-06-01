a94a46f4212e9751506675d339c1957ee18abf6f
fabiansj0028 <fabiansj0028@gmail.com>
2023-06-01 00:07:06 -0600

    Registro Completo #9

--- Android/BiblioTEC/app/src/main/java/com/example/bibliotec/ui/RegistroFragment.kt
+++ Android/BiblioTEC/app/src/main/java/com/example/bibliotec/ui/RegistroFragment.kt
@@ -2,6 +2,7 @@ package com.example.bibliotec.ui
 
 import androidx.fragment.app.Fragment
 import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -21,6 +22,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
+import java.util.*
 
 class RegistroFragment : Fragment() {
     private var _binding: FragmentRegistroBinding? = null
@@ -28,7 +30,7 @@ class RegistroFragment : Fragment() {
     private val binding get() = _binding!!
     private val gson = Gson()
     private lateinit var user : User
-
+    val selectedCalendar = Calendar.getInstance()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -44,7 +46,10 @@ class RegistroFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val editTextFechaNacimiento = view.findViewById<EditText>(R.id.editTextFechaNacimiento)
+        editTextFechaNacimiento.setOnClickListener{
+            OnClickFechaDate(view)
+        }
         binding.btnRegistro.setOnClickListener{
             val editTextNombre = view.findViewById<EditText>(R.id.editTextNombre)
             val editTextApellido1 = view.findViewById<EditText>(R.id.editTextApellido1)
@@ -52,11 +57,67 @@ class RegistroFragment : Fragment() {
             val editTextCedula = view.findViewById<EditText>(R.id.editTextCedula)
             val editTextCarnet = view.findViewById<EditText>(R.id.editTextCarnet)
             val editTextFechaNacimiento = view.findViewById<EditText>(R.id.editTextFechaNacimiento)
-            val editTextCorreo = view.findViewById<EditText>(R.id.editTextCorreo)
+            val editTextCorreo = view.findViewById<EditText>(R.id.editTextEmailRegistro)
             val editTextClaveRegistro = view.findViewById<EditText>(R.id.editTextClaveRegistro)
+
+            val nombre = editTextNombre.text.toString()
+            val apellido1 = editTextApellido1.text.toString()
+            val apellido2 = editTextApellido2.text.toString()
+            val cedula = editTextCedula.text.toString()
+            val carnet = editTextCarnet.text.toString()
+            val fechaNacimiento = editTextFechaNacimiento.text.toString()
+            val correo = editTextCorreo.text.toString()
+            val clave = editTextClaveRegistro.text.toString()
+
+            GlobalScope.launch(Dispatchers.IO) {
+                val url = "https://appbibliotec.azurewebsites.net/api/estudiante/crear"
+                val requestBody =
+                    ("{\"nombre\": \"${nombre}\"," +
+                            "\"apellido1\":\"${apellido1}\"," +
+                            "\"apellido2\":\"${apellido2}\"," +
+                            "\"cedula\":\"${cedula}\"," +
+                            "\"carnet\":\"${carnet}\"," +
+                            "\"fechaDeNacimiento\":\"${fechaNacimiento}\"," +
+                            "\"correo\":\"${correo}\"," +
+                            "\"clave\":\"${clave}\"}").toRequestBody("application/json".toMediaTypeOrNull())
+
+                val (responseStatus, responseString) = apiRequest.postRequest(url, requestBody)
+                if (responseStatus) {
+                    requireActivity().runOnUiThread() {
+                        AlertDialog.Builder(requireContext())
+                            .setTitle("Éxito")
+                            .setMessage("Registro exitoso")
+                            .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+                            .show()
+                        findNavController().navigateUp()
+                    }
+
+                } else {
+                    requireActivity().runOnUiThread() {
+                        AlertDialog.Builder(requireContext())
+                            .setTitle("Error")
+                            .setMessage(responseString)
+                            .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+                            .show()
+                    }
+                }
+            }
         }
     }
 
+    private fun OnClickFechaDate(view:View){
+
+        val fechaNacimiento = view.findViewById<EditText>(R.id.editTextFechaNacimiento)
+
+        val year = selectedCalendar.get(Calendar.YEAR)
+        val month = selectedCalendar.get(Calendar.MONTH)
+        val day = selectedCalendar.get(Calendar.DAY_OF_MONTH)
+        val listener = DatePickerDialog.OnDateSetListener{datePicker, y, m, d ->
+            selectedCalendar.set(y,m,d)
+            fechaNacimiento.setText("$y-$m-$d")
+        }
+        DatePickerDialog(requireContext(),listener,year,month,day).show()
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
