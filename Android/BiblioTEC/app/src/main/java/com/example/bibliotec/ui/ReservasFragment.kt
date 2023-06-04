ce64f32bfb14936046b30df53a9597bc0ce5395c
Erick <efmz2000@outlook.es>
2023-06-04 14:00:45 -0600

    a

--- Android/BiblioTEC/app/src/main/java/com/example/bibliotec/ui/ReservasFragment.kt
+++ Android/BiblioTEC/app/src/main/java/com/example/bibliotec/ui/ReservasFragment.kt
@@ -1,8 +1,10 @@
 package com.example.bibliotec.ui
 
+import android.app.AlertDialog
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import com.example.bibliotec.api.ApiRequest
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,12 +15,25 @@ import android.widget.ListView
 import android.widget.TextView
 import com.example.bibliotec.R
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class ReservasFragment : Fragment() {
     private lateinit var sharedPreferences: SharedPreferences
     private val gson = Gson()
     private var studentId: Int? = null
+    private lateinit var apiRequest: ApiRequest
+    data class Reserva(
+        val id: Int,
+        val nombre: String,
+        val fecha: String,
+        val horaInicio: String,
+        val horaFin: String,
+        val confirmado: Boolean
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,51 +44,116 @@ class ReservasFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inicializar sharedPreferences y studentId dentro de onCreateView
         sharedPreferences = requireContext().getSharedPreferences("UserInfo", Context.MODE_PRIVATE)
         studentId = sharedPreferences.getIntOrNull("studentId")
+        apiRequest = ApiRequest.getInstance(requireContext())
 
         val view = inflater.inflate(R.layout.fragment_reservas, container, false)
-
         val listViewReservas: ListView = view.findViewById(R.id.reserv_list)
+        val elementos: MutableList<String> = mutableListOf()
 
-        val elementos: MutableList<String> = mutableListOf("Elemento 1", "Elemento 2", "Elemento 3")
-
-        elementos.add(studentId.toString());
-
-        val adapter = object : ArrayAdapter<String>(
-            requireContext(),
-            R.layout.list_item_layout,
-            R.id.item_text,
-            elementos
-        ) {
-            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-                val view = super.getView(position, convertView, parent)
-
-                val itemText = view.findViewById<TextView>(R.id.item_text)
-                val buttonConfirmar = view.findViewById<Button>(R.id.button_confirmar)
-                val buttonEliminar = view.findViewById<Button>(R.id.button_eliminar)
-
-                // Acciones al hacer clic en el botón Confirmar
-                buttonConfirmar.setOnClickListener {
-                    // Acción al confirmar el elemento en la posición "position"
+        GlobalScope.launch(Dispatchers.IO) {
+            val url = "https://appbibliotec.azurewebsites.net/api/reserva/estudiante?id=$studentId"
+            val (responseStatus, responseString) = apiRequest.getRequest(url)
+            println("Se hizo la solicitud a $url")
+            println(responseStatus)
+            if (responseStatus) {
+                println("Se obtuvo la respuesta")
+                println(responseString)
+                val reservaType = object : TypeToken<List<Reserva>>() {}.type
+                val reservas: List<Reserva> = Gson().fromJson(responseString, reservaType)
+                for (reserva in reservas) {
+                    val elemento = "${reserva.nombre} Fecha: ${reserva.fecha.substring(0, 10)} de ${reserva.horaInicio.substring(11, 16)} a ${reserva.horaFin.substring(11, 16)}"
+                    println(elemento)
+                    elementos.add(elemento)
                 }
 
-                // Acciones al hacer clic en el botón Eliminar
-                buttonEliminar.setOnClickListener {
-                    // Acción al eliminar el elemento en la posición "position"
-                }
+                requireActivity().runOnUiThread {
+                    val adapter = object : ArrayAdapter<String>(
+                        requireContext(),
+                        R.layout.list_item_layout,
+                        R.id.item_text,
+                        elementos
+                    ) {
+                        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                            val view = super.getView(position, convertView, parent)
 
-                return view
+                            val itemText = view.findViewById<TextView>(R.id.item_text)
+                            val buttonConfirmar = view.findViewById<Button>(R.id.button_confirmar)
+                            val buttonEliminar = view.findViewById<Button>(R.id.button_eliminar)
+
+                            val reserva = reservas[position]
+                            itemText.text = elementos[position]
+
+                            // Acciones al hacer clic en el botón Confirmar
+                            buttonConfirmar.setOnClickListener {
+                                val confirmDialog = AlertDialog.Builder(requireContext())
+                                    .setTitle("Confirmación")
+                                    .setMessage("¿Estás seguro de confirmar esta reserva?")
+                                    .setPositiveButton("OK") { dialog, _ ->
+                                        confirmarReserva(reserva)
+                                        dialog.dismiss()
+                                    }
+                                    .setNegativeButton("Cancelar") { dialog, _ ->
+                                        dialog.dismiss()
+                                    }
+                                    .create()
+                                confirmDialog.show()
+                            }
+
+                            // Acciones al hacer clic en el botón Eliminar
+                            buttonEliminar.setOnClickListener {
+                                val deleteDialog = AlertDialog.Builder(requireContext())
+                                    .setTitle("Confirmación")
+                                    .setMessage("¿Estás seguro de eliminar esta reserva?")
+                                    .setPositiveButton("OK") { dialog, _ ->
+                                        eliminarReserva(reserva)
+                                        dialog.dismiss()
+                                    }
+                                    .setNegativeButton("Cancelar") { dialog, _ ->
+                                        dialog.dismiss()
+                                    }
+                                    .create()
+                                deleteDialog.show()
+                            }
+
+                            return view
+                        }
+                    }
+
+                    listViewReservas.adapter = adapter
+                }
+            } else {
+                println("Error al obtener las reservas")
+                println(responseString)
             }
         }
 
-        listViewReservas.adapter = adapter
-
         return view
+    }
+
+    private fun confirmarReserva(reserva: Reserva) {
+        val url = "https://appbibliotec.azurewebsites.net/api/reserva/confirmar" +
+                "?id=${reserva.id}&nombre=${reserva.nombre}&horaInicio=${reserva.horaInicio}&horaFin=${reserva.horaFin}"
+
+        println("URL de confirmación: $url")
+
+        // Realizar la solicitud HTTP para confirmar la reserva utilizando la URL generada
+        // y realizar las acciones necesarias después de la confirmación.
+    }
+
+    private fun eliminarReserva(reserva: Reserva) {
+        val url = "https://appbibliotec.azurewebsites.net/api/reserva/eliminar" +
+                "?id=${reserva.id}&nombre=${reserva.nombre}&horaInicio=${reserva.horaInicio}&horaFin=${reserva.horaFin}"
+
+        println("URL de eliminación: $url")
+
+        // Realizar la solicitud HTTP para eliminar la reserva utilizando la URL generada
+        // y realizar las acciones necesarias después de la eliminación.
     }
 
 
